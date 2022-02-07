eaaa599b010844fd96f7653b45db407e598a5eee
Moriah Maney <moriahmaney@gmail.com>
2022-02-07 15:10:23 -0500

    (style): updating css styles and refactoring

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,28 +1,37 @@
 import Image from 'next/image';
 import styles from '../styles/PokemonCard.module.css'
+import typeStyles from '../styles/TypeColors.module.css'
 
 export default function PokemonCard({pokemon}) {
-  const type = pokemon.types[0].type.name;
-  const typeBackgroundClass = styles[`${type}-gradient-background`];
-  const typeClass = styles[`${type}`];
-  const typeBorder = styles[`${type}-border`];
+  if(!pokemon) {
+    return;
+  }
+  const type = pokemon && pokemon.types && pokemon.types[0].type.name;
+  const typeBackgroundClass = typeStyles[`${type}-gradient-background`];
+  const typeClass = typeStyles[`${type}`];
+  const typeBorder = typeStyles[`${type}-border`];
 
   return (
       <figure key={pokemon.name} className={[styles.card, typeBackgroundClass].join(' ')}>
         <div className={styles.cardDetailContainer}>
           <div className={styles.pokeHeading}>
             <h2 className={styles.pokeName}>{pokemon.name}</h2>
-            <h3 className={[styles.type, typeClass].join(' ')}>{type}</h3>
+            <div className={styles.typeContainer}>
+              {pokemon.types?.map((type) => {
+                  const typeClass = typeStyles[`${type.type.name}`];
+                  return (<h3 key={type.type.name} className={[styles.type, typeClass].join(' ')}>{type.type.name}</h3>)
+              })}
+            </div>
           </div>
 
           <div className={styles.cardImageContainer}>
             <div className={[styles.imageBorder, typeBorder].join(' ')}>
-              <Image className={styles.cardImage} src={pokemon.sprites.front_default} alt={`${pokemon.name} default front-facing sprite image`} width={200} height={200} />
+              {pokemon.sprites?.front_default && <Image width={200} height={200} className={styles.cardImage} src={pokemon.sprites.front_default} alt={`${pokemon.name} default front-facing sprite image`} />}
             </div>
           </div>
           <table key={`${pokemon.name}-stats`}>
             <tbody>
-            {pokemon.stats.map((stats) => {
+            {pokemon.stats?.map((stats) => {
             return (
               <tr key={`${pokemon.name}-${stats.stat.name}`}>
                 <th className={styles.statName}>{stats.stat.name.replace('-', ' ')}</th>
